b41636984334a21e15a5911865269f44b0180cf3
Lukas Braun <mail@lukasbraun.com>
2018-07-27 18:01:22 +0200

    Add package docstring

---
+++ __init__.py
@@ -0,0 +1,20 @@
+"""
+Dragonfruitfly
+==============
+
+Provides
+    1. Real time stimulus creation
+    2. GPU accelerated model creation using TensorFlow
+    3. Real time visualization of stimuli and model responses
+
+
+Available packages
+------------------
+
+
+Utilities
+---------
+
+
+"""
+
