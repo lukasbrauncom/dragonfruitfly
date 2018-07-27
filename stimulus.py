494f4e04d2fa79fefe49be589c750a853809f7ea
Lukas Braun <mail@lukasbraun.com>
2018-07-27 18:55:46 +0200

    Add module to generate stimuli in real time

---
+++ stimulus.py
@@ -0,0 +1,13 @@
+"""
+Provides a framework to generate stimuli in real time.
+
+In order to reduce memory consumption and to enable very long and even stimuli 
+of infinite length, stimuli are render on the go.
+"""
+
+class Stimulus:
+    """Stimulus frame generator."""
+    
+    def __init__(self):
+        pass
+
