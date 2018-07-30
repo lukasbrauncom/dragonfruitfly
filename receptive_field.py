84ce910bec6e86034571bea6bd8f047067b9fc9c
Lukas Braun <mail@lukasbraun.com>
2018-07-30 21:28:49 +0200

    Add documentation and swap height and width parameters

--- receptive_field.py
+++ receptive_field.py
@@ -6,14 +6,21 @@ import numpy as np
 from scipy import stats
 
 def gaussian(size, ppu, sigma):
+    """Create 2D gaussian kernel
+    
+    Keyword arguments:
+    size -- Size as tuple(width, height)
+    ppu -- Pixel per unit
+    sigma -- Standard deviation of normal distribution
+    """
     gauss = stats.multivariate_normal([0, 0], [[sigma, 0], [0, sigma]])
     
     ss = 1 / ppu # Step size
     width = size[0] / 2
     height = size[1] / 2
     
-    x, y = np.mgrid[-width:width+ss:ss, -height:height+ss:ss]
-    pos = np.zeros((x.shape)+ (2,)))
+    x, y = np.mgrid[-height:height+ss:ss, -width:width+ss:ss]
+    pos = np.zeros((x.shape) + (2,))
     pos[:, :, 0] = x
     pos[:, :, 1] = y
     
